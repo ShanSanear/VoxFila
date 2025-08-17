b5760270063f97b0600a86da12994452ee452422
Shan <pruskimateusz93@gmail.com>
2025-08-17 19:09:11 +0200

    Added import button

--- app/src/views/songs_list.rs
+++ app/src/views/songs_list.rs
@@ -1,8 +1,10 @@
 use crate::components::icons::{IconLinkWithText, IconYtLinkWithText};
+use crate::components::ErrorModal;
 use crate::utils::{get_ising_search_link_for_song, get_yt_search_link_for_song};
-use ::server::songs::search_song_by_artist;
+use ::server::songs::{import_songs, search_song_by_artist};
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{debug, error, info};
+use shared::models::NewSong;
 
 use crate::views::Route;
 
@@ -27,8 +29,96 @@ pub fn TabButton(tab: String, current_tab: Signal<String>) -> Element {
 }
 
 #[component]
+pub fn ImportModal(open: Signal<bool>) -> Element {
+    let mut confirmed = use_signal(|| false);
+    let mut file_content = use_signal(|| String::new());
+    let mut error_open = use_signal(|| false);
+    let mut error_message = use_signal(|| String::new());
+    rsx! {
+
+        dialog { class: "modal", open: "{open}",
+            div { class: "modal-box",
+                input {
+                    r#type: "file",
+                    accept: ".json",
+                    class: "file-input file-input-bordered w-full max-w-xs",
+                    onchange: move |evt| {
+                        async move {
+                            if let Some(file_engine) = &evt.files() {
+                                let files = file_engine.files();
+                                let file_to_load = files.first();
+                                match file_to_load {
+                                    Some(file) => {
+                                        info!("Processing file: {}", file);
+                                        let input = file_engine.read_file_to_string(file).await;
+                                        match input {
+                                            Some(content) => {
+                                                debug!("File content: {}", content);
+                                                file_content.set(content);
+                                            }
+                                            None => {
+                                                debug!("Input file is empty: {file}",);
+                                                error_message.set("Input file is empty".to_string());
+                                                error_open.set(true);
+                                            }
+                                        }
+                                    }
+                                    None => {
+                                        debug!("Couldn't load file");
+                                    }
+                                }
+                            }
+                        }
+                    },
+                }
+                button {
+                    class: "btn btn-primary",
+                    onclick: move |_| async move {
+                        let new_songs: Result<Vec<NewSong>, serde_json::Error> = serde_json::from_str(
+                            file_content().as_str(),
+                        );
+                        match new_songs {
+                            Ok(songs) => {
+                                match import_songs(songs).await {
+                                    Ok(_) => {
+                                        info!("Successfully imported songs");
+                                    }
+                                    Err(e) => {
+                                        error!("Failed to import songs: {}", e);
+                                        error_message.set(format!("Failed to import songs: {}", e));
+                                        error_open.set(true);
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                error!("Failed to parse JSON: {}", e);
+                                error_message.set(format!("Failed to parse JSON: {}", e));
+                                error_open.set(true);
+                            }
+                        }
+                        confirmed.set(true);
+                    },
+                    "Submit"
+                
+                }
+                button {
+                    class: "btn btn-neutral",
+                    onclick: move |_| {
+                        confirmed.set(true);
+                        open.set(false);
+                    },
+                    "OK"
+                }
+            }
+        }
+        ErrorModal { open: error_open, message: error_message() }
+    }
+}
+
+#[component]
 pub fn SongsList() -> Element {
-    let mut current_tab = use_signal(|| "All songs".to_string());
+    let current_tab = use_signal(|| "All songs".to_string());
+    let mut open_import_modal = use_signal(|| false);
     //TODO can this be done without doing mut?
     let mut possible_tabs = vec!["All songs".to_string()];
     possible_tabs.extend(ASCII_UPPER.iter().map(|c| c.to_string()));
@@ -38,19 +128,28 @@ pub fn SongsList() -> Element {
         let search_query = if current_tab() == "All songs" {
             "".to_string()
         } else {
-            "{current_tab()}%".to_string()
+            format!("{}%", current_tab())
         };
         search_song_by_artist(search_query).await
     });
 
     rsx! {
+        ImportModal { open: open_import_modal }
         div { class: "h-full flex flex-col",
             div { class: "mb-4",
                 div { class: "flex flex-col sm:flex-row sm:items-center justify-between gap-4 mb-4",
-                    h2 { class: "text-2xl font-bold", "All Songs (17)" }
-                    div { class: "form-control w-full max-w-md",
-                        button { class: "btn btn-info", "Import songs" }
-                        div { class: "relative",
+                    h2 { class: "text-2xl font-bold", "All Songs" }
+                    div { class: "form-control w-full max-w-md flex items-center gap-2",
+                        button {
+                            class: "btn btn-info",
+                            onclick: move |_| {
+                                async move {
+                                    open_import_modal.set(true);
+                                }
+                            },
+                            "Import songs"
+                        }
+                        div { class: "relative flex-1",
                             input {
                                 class: "input input-bordered w-full pl-10",
                                 placeholder: "Search songs...",
