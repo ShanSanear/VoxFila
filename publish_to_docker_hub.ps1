8ab343fe43991f12b7231046f503f3a22962cfc9
Shan <pruskimateusz93@gmail.com>
2025-08-21 15:24:54 +0200

    Adding script to publish to docker

--- publish_to_docker_hub.ps1
+++ publish_to_docker_hub.ps1
@@ -1,6 +1,7 @@
+$VERSION="0.0.4"
 cargo sqlx prepare --workspace --all -- --all-targets --all-features
 git add .sqlx
 git commit -m "New sqlx prepare statements"
-docker build --tag "voxfila:0.0.3" .
-docker tag voxfila:0.0.3 shansanear/voxfila
+docker build --tag "voxfila:$VERSION" .
+docker tag "voxfila:$VERSION" shansanear/voxfila
 docker push shansanear/voxfila
