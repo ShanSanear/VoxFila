e49f54506d9e85be292724389f4bc53076b74b49
Shan <pruskimateusz93@gmail.com>
2025-08-22 12:16:03 +0200

    Added git tagging and push

--- publish_to_docker_hub.ps1
+++ publish_to_docker_hub.ps1
@@ -1,7 +1,11 @@
-$VERSION="0.0.4"
+$VERSION="0.0.5"
+$ErrorActionPreference = "Stop"
 cargo sqlx prepare --workspace --all -- --all-targets --all-features
 git add .sqlx
 git commit -m "New sqlx prepare statements"
 docker build --tag "voxfila:$VERSION" .
 docker tag "voxfila:$VERSION" shansanear/voxfila
 docker push shansanear/voxfila
+git push
+git tag -a "$VERSION" -m "Release $VERSION"
+git push --tags
